eca3c83e92da741c7aaebaa687c91137668f58f4
Ivanna <ivanna.kuliak@gmail.com>
2023-03-22 20:22:37 +0000

    change name of folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavTabs from './NavTabs.js';
-import Home from './pages/Home.js';
-import Moon from './pages/Moon.js'
+import Home from './Pages/Home.js';
+import Moon from './Pages/Moon.js'
 import StarsContainer from './components/StarsContainer';
 
 export default function App() {
